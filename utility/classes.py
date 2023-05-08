2fd012b8c71568884ddfd271b61bcbb5a38223a1
Advik <advik.b@gmail.com>
2023-05-08 13:58:31 +0530

    Update classes.py

--- utility/classes.py
+++ utility/classes.py
@@ -27,6 +27,15 @@ class FileHash:
             hasher.update(file.read())
             return hasher.hexdigest()
 
+    @staticmethod
+    def __inperfect_fsize(file_path: str, hasher: xxh64) -> str:
+        with open(file_path, 'rb') as file:
+            hasher.update(file.read(MAX_BUFFER_SIZE))
+            # Skip to the last part of the file and update the hasher with the last bit of data
+            file.seek(-MAX_BUFFER_SIZE, 2)
+            hasher.update(file.read())
+            return hasher.hexdigest()
+
     def __eq__(self, __value: object) -> bool:
         if isinstance(__value, FileHash):
             return self.file_hash == __value.file_hash
