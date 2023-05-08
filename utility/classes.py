b7ac3b5f1d1c1e36ab95ab590a4fe493fdb3bb1f
Advik <advik.b@gmail.com>
2023-05-08 13:47:55 +0530

    Create classes.py

---
+++ utility/classes.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from xxhash import xxh64
+from os import path
+from .settings import MAX_BUFFER_SIZE
+
+@dataclass
+class FileHash:
+    path: str
+    file_hash: str
+
+    @staticmethod
+    def from_file(file_path: str) -> 'FileHash':
+        hasher = xxh64()
+        file_size = path.getsize(file_path)
+        if file_size <= MAX_BUFFER_SIZE:
+            return FileHash(
+                file_hash=FileHash.__perfect_fsize(
+                    file_path=file_path,
+                    hasher=hasher,
+                ),
+                path=path.abspath(file_path)
+            )
+
+    @staticmethod
+    def __perfect_fsize(file_path: str, hasher: xxh64) -> str:
+        with open(file_path, 'rb') as file:
+            hasher.update(file.read())
+            return hasher.hexdigest()
